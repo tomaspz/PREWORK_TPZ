076d87f4394a29e705171d87057d680592599cb0
tomaspz <60239964+tomaspz@users.noreply.github.com>
2020-02-24 23:09:06 -0500

    Add files via upload

---
+++ Module-6/index.js
@@ -0,0 +1,17 @@
+document.getElementById("shrinkBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "25px";
+
+});
+
+document.getElementById("growBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("resetBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
---
+++ Module-7/scratch game.md
@@ -0,0 +1,5 @@
+Falling Mouse Shooting Cat
+
+by Tomas Perez
+
+https://scratch.mit.edu/projects/370700909
